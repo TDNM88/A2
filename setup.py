535ccef1fde54d4994049bd1f4d41ab9f51c969a
Nguyen DUng <Diable88@users.noreply.huggingface.co>
2024-05-12 06:16:53 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages
+
+name = "DRAWING WITH A.I"
+version = "0.1.0"
+
+packages = find_packages()
+
+dependencies = ["transformers>=4.11.3", "torch>=1.9.0", "pillow"]
+
+author = "TDN-M"
+author_email = "dung.ngt1988@gmail.com"
+description = "App for LLM Drawing Evaluation"
+long_description = "App that generates random themes, allows user drawing and evaluates drawings using LLM."
+url = "https://github.com/TDNM88"
+
+setup_requirements = ["setuptools>=40"]
+install_requires = dependencies
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Framework :: Transformers",
+]
